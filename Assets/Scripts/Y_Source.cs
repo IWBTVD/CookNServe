c12dd2256b787387f98694396eb339e6e5bd8e32
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2022-07-10 09:33:46 +0900

    as

--- Assets/Scripts/Y_Source.cs
+++ Assets/Scripts/Y_Source.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class Y_Source : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject Source;
-    public GameObject SourceOutPos;
+    public GameObject source;
+    public GameObject sourceOutPos;
+    public float sourceSpped;
+
+    private OVRGrabbable ovrGrabbable;
+
+    bool isDown;
     void Start()
     {
-        
+        ovrGrabbable = GetComponent<OVRGrabbable>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isDown){
+            Shot();
+        }
     }
+    void GetInput(){
+        isDown = OVRInput.Get(OVRInput.Button.One);
+    }
+    void Shot(){
+        GameObject intantSource = Instantiate(source);
+        intantSource.transform.position = sourceOutPos.transform.position;
+        Rigidbody sourceRigid = intantSource.GetComponent<Rigidbody>();
+        sourceRigid.velocity = sourceOutPos.transform.forward * sourceSpped;
+
+
+    }
+    
 }
