a2ca56363df527faedf7da302edd7f30ec7b4df4
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2022-07-22 09:02:39 +0900

    버거체크 임시구현
    테스트 필요

--- Assets/Scripts/Y_CheckBurger.cs
+++ Assets/Scripts/Y_CheckBurger.cs
@@ -4,7 +4,32 @@ using UnityEngine;
 
 public class Y_CheckBurger : MonoBehaviour
 {
-   public List<IngredientType> recipe1 = new List<IngredientType>(new IngredientType[]{});
+   public List<IngredientType> recipe1 = new List<IngredientType>();
 
+   bool isSame = false;
+   private void Start() {
+      planeBurger();
+   }
+   void planeBurger(){
+      recipe1.Add(IngredientType.BurnBottom);
+      recipe1.Add(IngredientType.CutletB);
+      recipe1.Add(IngredientType.Cabbage);
+      recipe1.Add(IngredientType.BurnTop);
+   }
 
+   private void OnTriggerEnter(Collider other) {
+      if(other.tag == "Hamburger"){
+         G_Hamburger enterBurger = other.GetComponent<G_Hamburger>();
+         isSame = recipe1.Equals(enterBurger.stackedIngredients);
+      }
+   }
+
+   private void Update() {
+      if(isSame){
+         Debug.Log("Same");
+      }
+      else{
+         Debug.Log("Diffrent");
+      }
+   }
 }
