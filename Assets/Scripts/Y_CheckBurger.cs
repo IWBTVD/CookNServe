96940e52b6d4ed59d3eb68d354f70edb0a0a3492
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2022-07-25 08:30:22 +0900

    11

--- Assets/Scripts/Y_CheckBurger.cs
+++ Assets/Scripts/Y_CheckBurger.cs
@@ -6,8 +6,7 @@ public class Y_CheckBurger : MonoBehaviour
 {
    public List<IngredientType> recipe1 = new List<IngredientType>(new IngredientType[]{});
 
-
-<<<<<<< HEAD
+   private bool isSame = false;
    private void OnTriggerEnter(Collider other) {
       if(other.tag == "Hamburger"){
          G_Hamburger enterBurger = other.GetComponent<G_Hamburger>();
@@ -21,6 +20,4 @@ public class Y_CheckBurger : MonoBehaviour
       }
    }
 
-=======
->>>>>>> parent of a2ca5636 (버거체크 임시구현)
 }
