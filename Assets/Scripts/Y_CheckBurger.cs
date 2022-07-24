427ad3fb870b529f58b01b97e36e953becd18339
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2022-07-25 08:59:11 +0900

    Update Y_CheckBurger.cs

--- Assets/Scripts/Y_CheckBurger.cs
+++ Assets/Scripts/Y_CheckBurger.cs
@@ -8,6 +8,7 @@ public class Y_CheckBurger : MonoBehaviour
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
    private void OnTriggerEnter(Collider other) {
       if(other.tag == "Hamburger"){
          G_Hamburger enterBurger = other.GetComponent<G_Hamburger>();
@@ -21,6 +22,8 @@ public class Y_CheckBurger : MonoBehaviour
       }
    }
 
+=======
+>>>>>>> parent of a2ca5636 (버거체크 임시구현)
 =======
 >>>>>>> parent of a2ca5636 (버거체크 임시구현)
 }
